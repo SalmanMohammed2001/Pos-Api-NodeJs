a4a30cfd904c6dc8896f6720d7ba2952f73f0e62
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-27 12:01:53 +0530

    mail sender  initialize

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,8 +1,12 @@
 const UserSchema=require('../model/UserSchema')
 const bcrypt=require('bcrypt')
+const jsonWebToken=require('jsonwebtoken')
+const nodeMailer=require('nodemailer')
 
-const login=(req,res)=>{
-    UserSchema.findOne({email:req.body.email}).then((result)=>{
+const register=(req,res)=>{
+
+
+   UserSchema.findOne({email:req.body.email}).then((result)=>{
         if(result==null){
 
             bcrypt.hash(req.body.password,10,function (err,hash) {
@@ -15,10 +19,34 @@ const login=(req,res)=>{
                         email:req.body.email,
                         activeState:req.body.activeState
                     })
-                    user.save().then((result)=>{
-                        res.status(200).json({message:'user saved',data:result})
-                    }).catch((er)=>{
-                        res.status(500).json(er)
+
+
+                    const transporter=nodeMailer.createTransport({
+                        service:'gmail',
+                        auth:{
+                            user:'umarmohammed20311@gmail.com',
+                            pass:"zeds xhhb ttjg ncdb"
+
+                        }
+                    });
+
+                    const mailOption={
+                        from:'umarmohammed20311@gmail.com',
+                        to:req.body.email,
+                        subject:'New Account create',
+                        text:'You Have created your account'
+                    }
+
+                    transporter.sendMail(mailOption,function (error,info){
+                        if(error){
+                            res.status(500).json({error:error})
+                        }else {
+                            user.save().then((result)=>{
+                                res.status(200).json({message:'user saved',data:result,info:info.response})
+                            }).catch((er)=>{
+                                res.status(500).json(er)
+                            })
+                        }
                     })
                 }
             })
@@ -26,9 +54,43 @@ const login=(req,res)=>{
         }else {
             return res.status(409).json({'error':'already exists!'});
         }
+    }).catch((er)=>{
+        return res.status(500).json(er);
+    })
+}
+
+const login=(req,res)=>{
+    UserSchema.findOne({email:req.body.email}).then((selectUser)=>{
+        if(selectUser!=null){
+
+            bcrypt.compare(req.body.password,selectUser.password, function(err, result) {
+
+                if(result){
+                    const playLoad={email:selectUser.email}
+                    const secretKey=process.env.SECRET_KRY;
+                    const   expiresIn='24h'
+
+                    const token=jsonWebToken.sign(playLoad,secretKey,{expiresIn})
+                    return res.status(200).json({token})
+
+                }else {
+                    res.status(401).json({message:'password incorrect'})
+                }
+
+
+            });
+
+         //   return res.status(500).json({data:selectUser});
+
+        }else {
+
+            res.status(409).json({message:'email not found'})
+        }
+    }).catch((er)=>{
+        return res.status(500).json(er);
     })
 }
 
 module.exports={
-    login
+    register,login
 }
--- router/UserRouter.js
+++ router/UserRouter.js
@@ -2,6 +2,7 @@ const UserController=require('../controller/UserController')
 const express=require('express')
 const router=express.Router();
 
-router.post("/signup",UserController.login)
+router.post("/signup",UserController.register)
+router.post("/login",UserController.login)
 
 module.exports=router
